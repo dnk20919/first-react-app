b044e3948c692bdd7363be5a4b81425f089bbef5
dnk20919 <daryashulpina@gmail.com>
2021-12-09 14:04:07 +0300

    added dynamics for checkbox

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react'
 import TodoList from './Todo/TodoList'
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'купить хлеб'},
     {id: 2, completed: false, title: 'купить масло'},
     {id: 3, completed: false, title: 'купить молоко'},
     {id: 4, completed: false, title: 'купить кофе'},
-  ]
+  ])
+  
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+      }
+        return todo
+      })
+    )
+  }
+
 
   return (<div className='wrapper'>
     <h1>React tutorial</h1>
 
-    <TodoList todos={todos} />
+    <TodoList todos={todos} onToggle={toggleTodo} />
   </div>)
 }
 
